46399383cc659ccf154f49d61ecab97ee8654562
Swaraj Behera <swaraj1933@gmail.com>
2017-10-11 00:56:24 +0530

    Update slack.js

--- conf/slack.js
+++ conf/slack.js
@@ -1,6 +1,6 @@
 let Slack = require('slack-node');
 
-let url = process.env.SLACK_WEBHOOK_URI;
+let url = 'https://hooks.slack.com/services/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';
 if (!url) { missing("SLACK_WEBHOOK_URI"); }
 
 let slack = new Slack();
